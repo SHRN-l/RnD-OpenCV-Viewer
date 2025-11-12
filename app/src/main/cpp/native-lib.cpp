204c5512b49b0c245959bda1f6a9704871c0a553
Sharan <sharanlokesh474@gmail.com>
2025-11-12 20:20:15 +0530

    Phase 1: Initial project setup and OpenCV C++ linkage

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,16 @@
-#include <jni.h>
 #include <string>
+#include <jni.h>
+// Add this line to test
+#include <opencv2/core.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_sharan_rnd_1opencv_1viewer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+
+    // Add this line to test
+    cv::Mat testMat;
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
